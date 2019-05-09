85f3852dc4a3e11265199f8f1ca9e2ee239b4a9c
yan <stanomy@qq.com>
2019-05-09 18:14:26 +0800

    1.更新页面解析

--- auto-chess/src/main/java/com/sy/autochess/spider/AutochessUser.java
+++ auto-chess/src/main/java/com/sy/autochess/spider/AutochessUser.java
@@ -1,10 +1,13 @@
 package com.sy.autochess.spider;
 
+import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
-import us.codecraft.webmagic.model.ConsolePageModelPipeline;
-import us.codecraft.webmagic.model.OOSpider;
-import us.codecraft.webmagic.model.annotation.ExtractBy;
-import us.codecraft.webmagic.model.annotation.TargetUrl;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Selectable;
+
+import java.util.List;
 
 /**
  * @Description TODO
@@ -12,14 +15,49 @@ import us.codecraft.webmagic.model.annotation.TargetUrl;
  * @Version 1.0
  * @Time 2019/5/8 13:34
  **/
-@TargetUrl("https://autochess\\.op\\.gg/user/[^-]+-\\d+")
-public class AutochessUser {
-    @ExtractBy("//h1[@style='margin-top: 10px;']/text()")
-    private String userName;
+public class AutochessUser implements PageProcessor {
+
+
+    private Site site = Site.me().setDomain("autochess.op.gg");
+
+    @Override
+    public void process(Page page) {
+        List<String> requests = page.getHtml().links().regex("https://autochess.op.gg/api/match/detail/\\d+?.+").all();
+        page.addTargetRequests(requests);
+//        page.putField("title", page.getHtml().xpath("//h1[@style='margin-top: 10px;']/text()"));
+//        if (page.getResultItems().get("title") == null) {
+//            page.setSkip(true);
+//        }
+        /**
+         *  -entry--win match-entry--top match-entry--
+         */
+        List<Selectable> list = page.getHtml().xpath("//table//tr").nodes();
+
+        for (Selectable s : list) {
+
+//            String id=s.xpath("/div/@id").get().replaceAll("entry-","");
+            String rank = s.xpath("//div[@class='\\\"rank\\\"']//span[@class='\\\"font-weight-bold\\\"']/text()").get();
+            String time = s.xpath("//div[@class='\\\"date text-muted\\\"']/@data-date").get();
+            System.out.println("当局等级=" + rank + ",游戏时间=" + time);
+            List<Selectable> heros = s.xpath("//div[contains(@class,'match-hero')]").nodes();
+            for (Selectable a : heros) {
+                String heroName = a.xpath("//img/@title").get();
+                int heroRank = a.xpath("//i[contains(@class,'fa fa-star')]").nodes().size();
+
+                System.out.println("英雄名=" + heroName + ",等级=" + heroRank);
+            }
+//            System.out.println(s.toString());
+        }
+//        page.putField("content",page.getHtml().xpath("//div[@id='neirong_box']/tidyText()"));
+    }
+
+    @Override
+    public Site getSite() {
+        return this.site;
+    }
 
     public static void main(String[] args) {
-//        Spider.create(new AutochessProcessor()).addUrl("https://autochess.op.gg/user/525387699-545948").pipeline(new ConsolePipeline()).run();
-        OOSpider.create(Site.me(), new ConsolePageModelPipeline(), AutochessUser.class).addUrl("https://autochess.op.gg/user/525387699-545948").run();
+        Spider.create(new AutochessUser()).addUrl("https://autochess.op.gg/api/match/detail/1598585?date=2019-05-08%2011:08:29").pipeline(new ConsolePipeline()).run();
 
     }
 }
